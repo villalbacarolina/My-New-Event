45c04d6a9819a1e6d445c75e73123f0f336a94c6
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2024-04-23 19:44:40 -0300

    config: spring boot base

---
+++ mynewevent/src/main/java/com/example/mynewevent/MyNewEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.mynewevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNewEventApplication.class, args);
+	}
+
+}
---
+++ mynewevent/src/test/java/com/example/mynewevent/MyNewEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mynewevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNewEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
